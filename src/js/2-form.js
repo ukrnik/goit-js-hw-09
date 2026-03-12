a09cf5d6725bc094fbb77d303315e65462bd4d6e
Nikita <152810933+ukrnik@users.noreply.github.com>
2026-03-12 22:10:48 +0100

    new task

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -1,1 +1,36 @@
-console.log('hello world');
+// const formEmail = document.querySelector('input[name="email"]');
+// const formMassage = document.querySelector('textarea[name="message"]');
+const form = document.querySelector('.feedback-form');
+
+let formData = {email: "", message: ""};
+
+if (localStorage.getItem("feedback-form-state") != {}) {
+    const userEmailData = JSON.parse(localStorage.getItem("userEmail"));
+    const userMassegeData = JSON.parse(localStorage.getItem("userMassage"));
+
+    formEmail.value = userEmailData;
+    formMassage.value = userMassegeData;
+    form.name.value =  
+}
+
+form.addEventListener('input', event => {
+    const userData = event.target.name.value.trim();
+    localStorage.setItem(feedback-form-state, JSON.stringify(userData));
+    console.log(userData);
+});
+
+// formMassage.addEventListener('input', event => {
+//     const userData = event.target.value;
+//     const userDataKey = 'userMassage';
+//     localStorage.setItem(userDataKey, JSON.stringify(userData));
+//     console.log(userData);
+// });
+
+form.addEventListener('submit', event => {
+    event.preventDefault();
+    localStorage.removeItem('userEmail');
+    localStorage.removeItem('userMassage');
+    form.reset();
+    console.log('Пусто');
+});
+
